51f56ee9d25ff6a13cbbd9551ff8bb5b5caafec9
Bjørn Drachmann <drachmann.bjorn@gmail.com>
2022-11-16 13:09:00 +0100

    Made Drone base class for Tello frameworks.

--- TestingFiles/TelloPySuper.py
+++ TestingFiles/TelloPySuper.py
@@ -49,31 +49,33 @@ class Drone(Tello):
         self.calibrated = False
         self.pos_x, self.pos_y, self.pos_z = 0, 0, 0
         self.vel_x, self.vel_y = 0, 0
+        self.pitch = 0
+        self.battery = 0
 
         #Grabbed from logdata
         self.battery = 0
 
         #Calibration
         self.calibrateCounter = 0
-        self.NOCTPET = 200               #Number of calibrations to perform each time
+        self.NOCTPET = 250               #Number of calibrations to perform each time
         self.avgACC = (0, 0, 0)
         self.avgROT = (0, 0, 0)
         self.avgVEL = (0, 0, 0)
         self.avgPOS = (0, 0, 0)
         self.CalibrateAcceleration()
+        self.standingStillCounter = 0
 
-        #Delete me
-        self.sum1 = 0
-        self.sum2 = 0
-        self.sum3 = 0
+        self.dillerx = []
+        self.dillery = []
 
 
     def CalibrateAcceleration(self):
         self.calibrateCounter = self.NOCTPET
+        self.calibrated = False
 
 
     
-    lastResponse, lastAcceleration = 0, 0
+    lastResponseTime, lastDataRecieved = 0, 0
     def dataHandler(self, event, sender, data, **args):
         if event == Tello.EVENT_FLIGHT_DATA:
             #drone = sender
@@ -82,10 +84,10 @@ class Drone(Tello):
             
         
         if event == Tello.EVENT_LOG_DATA:
-            t = time() - self.lastResponse
+            t = time() - self.lastResponseTime
 
             #Only calculate if new values and if time is reasonable
-            if int(t) < 5:# and data.imu.acc_x != self.lastAcceleration:
+            if int(t) < 5:# and data.imu.acc_x != self.lastDataRecieved:
                 
                 if not self.calibrated:
                     if self.calibrateCounter % 50 == 0:
@@ -96,14 +98,14 @@ class Drone(Tello):
                     az = data.imu.acc_z
                     rr, rp, ry = euler_from_quaternion(data.imu.q0, data.imu.q1, data.imu.q2, data.imu.q3)
 
-                    if 200 >= self.calibrateCounter > 100: #ACCELERATION AND ROTATION
+                    if 250 >= self.calibrateCounter > 150: #ACCELERATION AND ROTATION
                         nor = 100 # number of runs
                         self.avgACC = (self.avgACC[0] + data.imu.acc_x/nor, self.avgACC[1] + data.imu.acc_y/nor, self.avgACC[2] + data.imu.acc_z/nor)
                         self.avgROT = (self.avgROT[0] + rr/nor, self.avgROT[1] + rp/nor, self.avgROT[2] + ry/nor)
                         #print("avgACC %20s avgROT %20s." % (self.avgACC, self.avgROT))
 
-                    elif 100 >= self.calibrateCounter > 50:  #VELOCITY
-                        nor = 50
+                    elif 150 >= self.calibrateCounter > 50:  #VELOCITY
+                        nor = 100
                         fax = ax - self.avgACC[0]
                         fay = ay - self.avgACC[1]
                         roll, pitch, yaw = euler_from_quaternion(data.imu.q0, data.imu.q1, data.imu.q2, data.imu.q3)
@@ -154,16 +156,23 @@ class Drone(Tello):
                     rfax = fax - (math.cos(math.radians(90-fpitch))) * self.avgACC[2]
                     rfay = fay - (math.cos(math.radians(90-fyaw))) * self.avgACC[2]
 
-                    tolerance = 0.0025
-                    if rfax - tolerance > 0 or 0 > rfax + tolerance:
-                        self.vel_x = ((rfax * t) - self.avgVEL[0]) + self.vel_x
+                    tolerance = 0.006
+                    if (rfax - tolerance > 0 or 0 > rfax + tolerance) and (rfay - tolerance > 0 or 0 > rfay + tolerance):
+                        self.vel_x = ((rfax * t) + self.vel_x) - self.avgVEL[0]
+                        self.vel_y = ((rfay * t) + self.vel_y) - self.avgVEL[1]
+                        #print("Accelerated")
+                        self.standingStillCounter = 0
                     else:
-                        self.vel_x = 0
-                    
-                    if rfay - tolerance > 0 or 0 > rfay + tolerance:
-                        self.vel_y = ((rfay * t) - self.avgVEL[1]) + self.vel_y
-                    else:
-                        self.vel_y = 0
+                        self.standingStillCounter += 1
+                        print(50*"-" + "STANDING STILL")
+
+                        if self.standingStillCounter >= 10:
+                            self.vel_x = 0
+                            self.vel_y = 0
+
+                            self.standingStillCounter = 0
+                            print(60*"-" + "RESET VELOCITY")
+                        #print("Same")
                     
                     self.pos_x = (0.5 * rfax * t**2) + (self.vel_x * t) + self.pos_x
                     self.pos_y = (0.5 * rfay * t**2) + (self.vel_y * t) + self.pos_y
@@ -171,14 +180,19 @@ class Drone(Tello):
                     self.pos_x -= self.avgPOS[0]
                     self.pos_y -= self.avgPOS[1]
 
-                    #print("ACCEL X %8f   Y %8f    Z %8f" % (abs(rfax), abs(rfay), az))
+                    #print("ACCEL X %8f   Y %8f    Z %8f" % (abs(rfax*100), abs(rfay*100), az))
                     #print("VEL X %8f   Y %8f    Z %8f" % (self.vel_x, abs(self.vel_y), az))
                     print("COORDS X %2f   Y %2f    Z %8f" % (round(self.pos_x*100, 2), round(self.pos_y*100, 2), self.pos_z))
-            else:
-                print("INVALID")
 
-            self.lastResponse = time()
-            self.lastAcceleration = data.imu.acc_x
+
+                    self.pitch = fpitch
+                    self.dillerx.append(self.pos_x*100)
+                    self.dillery.append(self.pos_y*100)
+
+
+
+            self.lastResponseTime = time()
+            self.lastDataRecieved = data.imu.acc_x
     
 
     def goUp(self, centimeter):
@@ -191,15 +205,34 @@ class Drone(Tello):
 
 if __name__ == "__main__":
 
-    drone1 = Drone(ip="192.168.137.56", port=9001)
+    drone1 = Drone(ip="192.168.137.146", port=9001)
     
     print("OBJECT MADE")
-    for i in range(100):
-        #print("Drone battery =", drone1.tello)
-        drone1.tello.land()
-        #print(drone1.tello.state)
+    for i in range(50):
+        if drone1.calibrated == True:
+            break
+        print("Still sleeping")
         sleep(1)
     
+    
+    drone1.tello.takeoff()
+    sleep(5)
+    print("FORWARD")
+    drone1.tello.forward(10)
+    sleep(5)
+    print("BACKWARD")
+    drone1.tello.backward(10)
+    sleep(5)
+    print("LAND")
+    drone1.tello.land()
+    print("PLOTTING IN 5 SECONDS")
+    sleep(5)
+    print("PLOTTING NOW")
+    import matplotlib.pyplot as plt
+    plt.plot(drone1.dillerx, drone1.dillery, 'bo', label="PATH")
+    plt.legend()
+    plt.show()
+
 
     #Swarm kan bruge
     drone1.pos_x        #Position
