8a39c2da379312b86e9cf951c9e576e442f93715
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2022-12-03 11:49:25 +0100

    Updated batch script with error handling

--- run.bat
+++ run.bat
@@ -1,15 +1,47 @@
 @echo off
 
 :: @==========================Install required packages=============================@ ::
-echo "Installing required packages..."
-pip install -r requirements.txt
-
-echo "Done installing..."
+ECHO "Installing required packages..."
+CALL pip install -r requirements.txt
+IF errorlevel 1 GOTO ERROR
+ECHO Successfully installed all packages located inside requirements.txt
+GOTO PS
 
 :: @==========================Run our python script=============================@ ::
+:PS
 set path=%~dp0
 
 set absolute=%path%main.py
 echo Found main.py path %absolute%
 
 "C:\Python310\python.exe" %absolute%
+
+
+:: @==========================Error Handling=============================@ ::
+:ERROR
+ECHO Locating the error...
+for /F "" %%x in (requirements.txt) do (
+    echo %%x|find "#" >nul
+    if errorlevel 1 (
+        timeout /T 1 /NOBREAK
+        echo Package file   %%x
+
+        SET str = pygame
+        if %%x == %str% (
+            :: Try to download the fixed version of pygame
+            ECHO pip install pygame
+            if errorlevel 1 (
+                ECHO Found an issue when trying to install pygame 
+                ECHO Trying to fix the issue...
+                CALL pip install pygame --pre
+                if errorlevel1 (
+                    ECHO Cannot fix the issue.
+                    timeout /T 1 /NOBREAK
+                ) else (
+                    ECHO Fixed the issue and successfully installed pygame
+                    timeout /T 1 /NOBREAK
+                )
+            )
+        )
+    ) 
+)
